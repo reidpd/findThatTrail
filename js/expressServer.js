f4124aac4941eb8206d2d959c38364837e946112
Reid Delahunt <reidpierredelahunt@gmail.com>
2017-02-27 17:16:30 -0800

    finding the right path

--- js/expressServer.js
+++ js/expressServer.js
@@ -3,7 +3,7 @@ let app = express();
 let path = require('path');
 let trailPath = path.join(__dirname, 'html/index.html');
 
-app.use(trailPath, express.static('.'));
+app.use(express.static('.'));
 
 app.listen((process.env.PORT || 8000), () => {
   console.log("Listening on port 8000");
